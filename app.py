6be251853b3530b0f6200e07c40524879f285968
Brian Dorton <brian.dorton@ibm.com>
2020-02-04 13:50:15 -0600

    add dynamic mondays

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from apiserver import ApiServer, ApiRoute
+import arrow
 import requests
 import json
 
@@ -7,9 +8,15 @@ class MyServer(ApiServer):
        
         @ApiRoute("/lunch")
         def getLunch(req):
-            
-            r = requests.get('https://webapis.schoolcafe.com/api/CalendarView/GetWeeklyMenuitems?SchoolId=4e1c1aa8-8fbe-49b7-af51-61a61f0bc651&ServingDate=02%2F3%2F2020&ServingLine=Line%201&MealType=Lunch')
+            monday = arrow.utcnow().shift(weeks=-1, weekday=0).format('MM-DD-YYYY')
+            menu_params = 'GetWeeklyMenuitems'
+            meal_type = 'Lunch'
+            school_id = '4e1c1aa8-8fbe-49b7-af51-61a61f0bc651'
 
+            url = f'https://webapis.schoolcafe.com/api/CalendarView/{menu_params}?SchoolId={school_id}&ServingDate={monday}&ServingLine=Line%201&MealType={meal_type}'
+
+            r = requests.get(url)
             return r.json()
+            
 
 MyServer("127.0.0.1",8888).serve_forever()
